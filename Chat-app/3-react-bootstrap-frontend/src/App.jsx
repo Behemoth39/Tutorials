b9a8766a6b40d03db0330878ff6dad2c3bf0dd08
Behemoth39 <simme_jonsson@hotmail.com>
2023-06-22 12:43:01 +0200

    Contacts doesn't work

--- Chat-app/3-react-bootstrap-frontend/src/App.jsx
+++ Chat-app/3-react-bootstrap-frontend/src/App.jsx
@@ -2,11 +2,18 @@ import React from "react";
 import Login from "./components/Login";
 import Dashboard from "./components/Dashboard";
 import useLocalStorage from "./hooks/useLocalStorage";
+import { ContactsProvider } from "./context/ContactsProvider";
 
 const App = () => {
   const [id, setId] = useLocalStorage("id");
 
-  return id ? <Dashboard id={id} /> : <Login onIdSubmit={setId} />;
+  const dashboard = (
+    <ContactsProvider>
+      <Dashboard id={id} />
+    </ContactsProvider>
+  );
+
+  return id ? dashboard : <Login onIdSubmit={setId} />;
 };
 
 export default App;
---
+++ Chat-app/3-react-bootstrap-frontend/src/components/NewConversationModal.jsx
@@ -0,0 +1,7 @@
+import { Modal } from "react-bootstrap";
+
+const NewConversationModal = ({ closeModal }) => {
+  return <div>NewConversationModal</div>;
+};
+
+export default NewConversationModal;
